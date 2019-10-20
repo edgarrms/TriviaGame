d0f941e92e15a7394ffeb828f4863e25f7a68ac2
Edgar <ramosjair94@gmail.com>
2019-10-19 20:33:34 -0700

    added the rest of the code, trying to figure out why it will not run

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,6 +10,7 @@ var optionA = document.getElementById("a");
 var optionB = document.getElementById("b");
 var optionC = document.getElementById("c");
 var optionD = document.getElementById("d");
+var counter = document.getElementById("counter");
 var timeGauge = document.getElementById("timeGauge");
 var scoreDiv = document.getElementById("scoreContainer");
 
@@ -51,8 +52,15 @@ let questions = [{
 }
 ];
 
-var finalQuestion = question.length - 1;
+var finalQuestion = questions.length - 1;
 let questionRun = 0;
+let count = 0;
+var questionCountDown = 20;
+var gaugeWidth = 150;
+var gaugeUnit = gaugeWidth / questionCountDown;
+let timer;
+let score = 0
+
 
 function rndQustion() {
     let q = questions[questionRun];
@@ -64,6 +72,68 @@ function rndQustion() {
     optionD.innerHTML = q.optionD;
 }
 
-StaticRange.style.display = "none";
-rndQustion();
-quiz.style.display = "block";
+function startQuiz() {
+    Start.style.display = "none";
+    rndQustion();
+    quiz.style.display = "block";
+    rndProgress();
+    rndCounter();
+    timer = setInterval(rndCounter, 1000);
+}
+
+function rndProgress() {
+    for (let qIndex = 0; qIndex <= finalQuestion; qIndex++) {
+        ProgressEvent.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
+    }
+}
+
+function rndCounter() {
+    if (count <= questionCountDown) {
+        rndCounter.innerHTML = count;
+        timeGauge.style.width = count * gaugeUnit + "px";
+        count++
+    }
+    else {
+        count = 0
+        wrongAnswer();
+        if (questionRun < finalQuestion) {
+            questionRun++;
+            rndQustion();
+        }
+        else {
+            clearInterval(timer);
+            scoreRnd();
+        }
+    }
+}
+
+function answerCheck(answer) {
+    if (answer == questions[questionRun].correct) {
+        score++;
+        correctAnswer();
+    }
+    else {
+        wrongAnswer();
+    }
+    count = 0;
+    if (questionRun < finalQuestion) {
+        questionRun++;
+        rndQustion();
+    }
+    else {
+        clearInterval(timer);
+        scoreRnd();
+    }
+}
+
+function correctAnswer() {
+    document.getElementById(questionRun).style.backgroundColor = "black";
+}
+
+function wrongAnswer() {
+    document.getElementById(questionRun).style.backgroundColor = "red";
+}
+function scoreRnd() {
+    scoreDiv.style.display = "block";
+    var scoreIs = Math.round(100 * score / questions.length);
+}
